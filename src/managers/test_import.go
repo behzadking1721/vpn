a0c9f07068c20ad122d6226f1ce61ee5c54544f5
behza <behza@example.com>
2025-11-03 00:40:12 +0330

    refactor(alert): remove alert manager and related components
    - Remove AlertManager struct and its related methods
    - Delete alert evaluation logic for data usage and ping
    - Remove alert rule handling functions
    - Delete Alert and AlertRule models
    - Remove WebSocketAlertHandler
    - Update imports and dependencies

--- src/managers/test_import.go
+++
@@ -1,9 +0,0 @@
-package managers
-
-import (
-	_ "vpn-client/src/core"
-	_ "vpn-client/src/protocols"
-	_ "vpn-client/src/utils"
-)
-
-// This file is only for testing imports
--- src/test.go
+++
@@ -1,6 +0,0 @@
-package main
-
-import _ "vpn-client/src/core"
-
-func main() {
-}
